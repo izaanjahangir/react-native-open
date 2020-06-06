305b694be1a310cff516be950f9f1b834cc04555
izaanjahangir <izaanjahangir2@gmail.com>
2020-06-06 18:55:51 +0500

    added linkedin and twitter

--- src/helper.js
+++ src/helper.js
@@ -12,3 +12,29 @@ export async function openFacebook(url) {
     throw e;
   }
 }
+
+export async function openLinkedin(url) {
+  try {
+    const supported = await Linking.canOpenURL(url);
+
+    if (!supported) throw new Error("Linkedin is not available");
+
+    return Linking.openURL(url);
+  } catch (e) {
+    throw e;
+  }
+}
+
+export async function openTwitter(rawUrl = "") {
+  try {
+    const username = rawUrl.split("com/")[1];
+    const url = `twitter://user?screen_name=${username}`;
+    const supported = await Linking.canOpenURL(url);
+
+    if (!supported) throw new Error("Twitter is not available");
+
+    return Linking.openURL(url);
+  } catch (e) {
+    throw e;
+  }
+}
--- src/lib.js
+++ src/lib.js
@@ -1,5 +1,7 @@
-import { openFacebook } from "./helper";
+import { openFacebook, openLinkedin, openTwitter } from "./helper";
 
 export function openExternalApp(name, url) {
   if (name === "facebook") return openFacebook(url);
+  if (name === "linkedin") return openLinkedin(url);
+  if (name === "twitter") return openTwitter(url);
 }
